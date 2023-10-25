9620815ab098657e0db9c2b27e969175d648c29d
tirthadas9775 <75427198+tirthadas9775@users.noreply.github.com>
2023-10-25 11:06:09 +0600

    product section api and user collection created

--- index.js
+++ index.js
@@ -27,27 +27,58 @@ async function run() {
     await client.db("admin").command({ ping: 1 });
     const brands = client.db('tech-hunt').collection('brands');
     const productCollection = client.db('tech-hunt').collection('productCollection');
+    const userCollection = client.db('tech-hunt').collection('userCollection');
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
     app.get('/brands', async (req, res) => {
       const cursor = brands.find();
       const result = await cursor.toArray();
       res.send(result);
     });
+    app.get('/products', async (req, res) => {
+      const cursor = productCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+    app.get('/products/:name', async (req, res) => {
+      const name = req.params.name;
+      const query = { brand: `${name.charAt(0).toUpperCase()}${name.slice(1, name.length)}` }
+      const cursor = productCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get('/product/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await productCollection.findOne(query);
+      res.send(result);
+    });
+    app.get('/users/:uid', async(req, res) => {
+      const id = req.params.uid;
+      const query = {uid: id}
+      const result = await userCollection.findOne(query);
+      res.send(result);
+    })
+    app.patch('/users/:uid', async(req, res) => {
+      const uid = req.params.uid;
+      const query = {uid : uid}
+      const newItem = req.body;
+      const updatedItem = {
+        $addToSet : {
+          "items" : {name: newItem.name, brand: newItem.brand, photo: newItem.photo, price: newItem.price, quantity: newItem.quantity}
+        }
+      }
+      const result = await userCollection.updateOne(query, updatedItem);
+      res.send(result);
+    })
+
     app.post('/products', async (req, res) => {
       const newProduct = req.body;
       const result = await productCollection.insertOne(newProduct);
       res.send(result);
     });
-    app.get('/products', async(req, res) => {
-      const cursor = productCollection.find();
-      const result = await cursor.toArray();
-      res.send(result);
-    })
-    app.get('/products/:name', async(req, res) => {
-      const name = req.params.name;
-      const query = {brand : `${name.charAt(0).toUpperCase()}${name.slice(1, name.length)}`}
-      const cursor = productCollection.find(query);
-      const result = await cursor.toArray();
+    app.post('/users', async(req, res) => {
+      const newUser = req.body;
+      const result = await userCollection.insertOne(newUser);
       res.send(result);
     })
   } finally {
